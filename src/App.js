ceed455a6eaa6adf7920945179d746e2d0eba368
camkol <koliyus3@hotmail.com>
2024-08-05 19:51:11 +0200

    start router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navigation />
       <HomePage />
-    </div>
+    </Router>
   );
 }
 
